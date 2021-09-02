bea63814834422234422b835645612e400a7504e
Matthew King <mking@mking.net>
2021-09-02 10:02:46 +0800

    Update copyright years and version number.

--- build/build-library.ps1
+++ build/build-library.ps1
@@ -1,4 +1,4 @@
 $root = Resolve-Path (Join-Path $PSScriptRoot "..")
 $project = "$root/src/VersionGenerator/"
 $output = "$root/artifacts/"
-dotnet pack --configuration Release --output "$output" /p:Version=0.1.0 "$project"
+dotnet pack --configuration Release --output "$output" /p:Version=0.2.0 "$project"
