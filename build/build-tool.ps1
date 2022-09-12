5734ff13e8ae0a31cf8c8e2c558ca38d5eb0a6e9
Matthew King <mking@mking.net>
2022-09-12 10:29:19 +0800

    Increment tool version.

--- build/build-tool.ps1
+++ build/build-tool.ps1
@@ -1,4 +1,4 @@
 $root = Resolve-Path "$PSScriptRoot/.."
 $project = "$root/src/VersionGenerator.Tool/"
 $output = "$root/artifacts/"
-dotnet pack --configuration Release --output "$output" /p:Version=0.2.0 "$project"
+dotnet pack --configuration Release --output "$output" /p:Version=0.3.0 "$project"
